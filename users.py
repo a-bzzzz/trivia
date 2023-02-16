a70047b4c023efe8743cf2aedea75c5f75f3a316
Asta Björk <asta.bjork@helsinki.fi>
2023-02-16 18:23:22 +0200

    users.py: add get_usernames() & add_admin(),small fixes

--- users.py
+++ users.py
@@ -1,8 +1,6 @@
-from app import app
 from db import db
-from flask import abort, request, redirect, render_template, session
+from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
-import os
 from sqlalchemy.sql import text
 
 def login(username, password):
@@ -15,7 +13,7 @@ def login(username, password):
         if user.role_id == 1 or user.role_id == 2 or user.role_id == 3:
             if not check_password_hash(user.password, password):
                 return False
-        session["user_id"] 	    = user.id
+        session["user_id"] 	= user.id
         session["user_name"] 	= user.username
         session["user_role"] 	= user.role_id
         return True
@@ -25,7 +23,6 @@ def register(username, password):
     try:
         sql = """INSERT INTO users (username, password)
             VALUES (:username, :password)"""
-
         db.session.execute(text(sql), {"username":username, "password":hash_value})
         db.session.commit()
     except:
@@ -63,3 +60,18 @@ def user_visible():
 
 def user_created():
     return session.get("user_created", None)
+
+def get_usernames():
+    sql = """SELECT username FROM users"""
+    result = db.session.execute(text(sql))
+    return result.fetchall()
+
+def add_admin():
+    admin 		= "admin"
+    admin_password 	= "SalaSana12"
+    admin_role		= 1
+    hash_value 		= generate_password_hash(admin_password)
+    sql = """INSERT INTO users (username, password, role_id) 
+        	VALUES (:username, :password, :role_id)"""
+    db.session.execute(text(sql), {"username":admin, "password":hash_value, "role_id":admin_role})
+    db.session.commit()
