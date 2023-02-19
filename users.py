3944d31e05c1ca34d83b334eb84c986574473bcd
Asta Björk <asta.bjork@helsinki.fi>
2023-02-19 21:06:12 +0200

    Refactor users.py: Add is_admin(), check_password(), change_password() for password handling

--- users.py
+++ users.py
@@ -64,17 +64,42 @@ def user_visible():
 def user_created():
     return session.get("user_created", None)
 
+def is_admin():
+    return user_id() == 1
+
 def get_usernames():
     sql = """SELECT username FROM users"""
     result = db.session.execute(text(sql))
     return result.fetchall()
 
+def check_password(username, password):
+    sql = """SELECT id, username, password, role_id FROM users WHERE username=:username"""
+    result = db.session.execute(text(sql), {"username":username})
+    user = result.fetchone()
+    if not check_password_hash(user.password, password):
+        return False
+    return True
+
+def change_password(username, old_password, new_password):
+    if not is_admin():
+        if old_password == user_password():
+            return False
+    password = generate_password_hash(new_password)
+
+    try:
+        sql = """UPDATE users SET password=:password WHERE username=:username"""
+        db.session.execute(text(sql), {"username":username, "password":password})
+        db.session.commit()
+        return True
+    except Exception as error:
+        return error
+
 def add_admin():
-    admin 		    = "admin"
+    admin 		= "admin"
     admin_password 	= "salasana"
     admin_role		= 1
     hash_value 		= generate_password_hash(admin_password)
-    sql = """INSERT INTO users (username, password, role_id) 
+    sql = """INSERT INTO users (username, password, role_id)
         	VALUES (:username, :password, :role_id)"""
     db.session.execute(text(sql), {"username":admin, "password":hash_value, "role_id":admin_role})
     db.session.commit()
